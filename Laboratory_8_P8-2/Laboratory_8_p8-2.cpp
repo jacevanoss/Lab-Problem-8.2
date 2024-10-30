2155f2a7330465864cc134059f78b00fc9545cf7
Jace Van Oss <j-vanoss@onu.edu>
2024-10-30 11:29:58 -0400

    completed countWords function

--- Laboratory_8_P8-2/Laboratory_8_p8-2.cpp
+++ Laboratory_8_P8-2/Laboratory_8_p8-2.cpp
@@ -1,1 +1,44 @@
- 
+/* Date: 10/24/2024
+* Name: Jace Van Oss
+* File: Laboratory_8_P8-2.CPP
+*
+* Description:  program that counts the number of words in a string from the command line
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countWords(string str);
+
+int main() {
+
+	string test = "Hello world";
+	cout << countWords(test);
+	
+
+	return 0;
+}
+
+int countWords(string str) {
+	int count = 1;
+	bool word = true;
+	for (int i = 0; i < str.length(); i++) {
+		int num1 = i;
+		int num2 = 1;
+		str.substr(num1, num2);
+		if (str.substr(num1, num2) != " ") {
+			if (!word) {
+				word = true;
+				count++;
+			}
+		}
+		else {
+			word = false;
+		}
+		num1++;
+		num2++;
+	}
+	return count;
+}
